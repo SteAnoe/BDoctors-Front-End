e9a4acd30153ccd24a04c99c5f5b2b0a4f293478
Oksana Karpyn <oksana101992@gmail.com>
2023-07-21 13:07:56 +0200

    push

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -13,13 +13,13 @@ export default {
             specializations: null,
             selectedSpecializations: [],
 
+
         }
     },
     created() {
 
     },
     computed: {
-        // filteredDoctor();
     },
     mounted() {
         this.getApiDoctor();
@@ -37,17 +37,15 @@ export default {
             const params = {
                 page: doctorApiPage
             }
-
-            // if (this.selectedSpecializations !== 'all') {
-            //     params.specialization_id = this.selectedSpecializations
-            // }
             if (this.selectedSpecializations.length > 0) {
                 params.specializations_ids = this.selectedSpecializations.join(',');
             }
 
+
             axios.get(`${this.baseUrl}/api/doctors`, { params })
                 .then((res) => {
                     this.doctors = res.data.doctors
+
                 })
         },
         getApiSpecializations() {
@@ -62,13 +60,6 @@ export default {
 </script>
 <template>
     <h1>Ciao mondo</h1>
-    <!-- <div class="mb-3">
-        <label for="" class="form-label">Specializations Filter</label>
-        <select @change="getApiDoctor()" v-model="selectedSpecializations" class="form-select form-select-lg" name="" id="">
-            <option value="all"> -- All -- </option>
-            <option v-for="(elem, index) in specializations" :key="index" :value="elem.id">{{ elem.name }}</option>
-        </select>
-    </div> -->
     <div class="mb-3">
         <h2>Technologies Filter</h2>
         <label for="" v-for="(elem, index) in specializations" :key="index">
@@ -76,9 +67,18 @@ export default {
             {{ elem.name }}
         </label>
     </div>
+    <!-- <div class="mb-3">
+        <h2>Filter by Reviews</h2>
+        <label for="minimumRating">Minimum Rating:</label>
+        <input type="number" v-model="minimumRating" id="minimumRating" min="1" max="5">
+
+        <label for="minimumReviews">Minimum Number of Reviews:</label>
+        <input type="number" v-model="minimumReviews" id="minimumReviews" min="0">
+    </div> -->
     <div>
         <div v-for="(doctor, index) in doctors" :key="index">
             <h2>{{ doctor.name }}</h2>
+
             <ul>
                 <span class="text-bold">Specializations:</span>
                 <li v-for="(specialization, index) in doctor.specializations" :key="index">{{ specialization.name }}</li>
